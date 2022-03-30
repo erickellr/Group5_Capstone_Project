80317cfb8a65d24a40d4173c654820c05d3c2263
Eric Keller <74887733+erickellr@users.noreply.github.com>
2022-03-30 00:18:12 +0000

    Welcome page version 1: Skeleton sign in ability, javascript stores user information on click of sign in and create account

---
+++ welcome.js
@@ -0,0 +1,5 @@
+function storeUserInformation(){
+  let userID = document.getElementById("userID").value;
+  let userPass = document.getElementById("userPass").value;
+  console.log($"Password: {userID}, userPass)
+}
