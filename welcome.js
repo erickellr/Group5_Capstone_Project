e8d3009e2724b910a3586745c75561d0d71f388c
Eric Keller <74887733+erickellr@users.noreply.github.com>
2022-03-30 08:37:01 +0000

    Working PHP login ID check/creation

--- welcome.js
+++ welcome.js
@@ -1,13 +1,23 @@
+  async function callBackend() {
 
-async function storeUserInfo(){
-  console.log("inside function")
-    let myResponse = await fetch("userInfo.php", {
+      loginID = document.getElementById('loginID').value;
+      password = document.getElementById('password').value;
+      var alphaNumericRegEx = /^[0-9a-zA-Z]+$/;
+      if (!loginID.match(alphaNumericRegEx)) {
+	  document.getElementById('output1').innerHTML = '<span style=color:red;>Login ID must consist of letters and numbers only</span>';
+	  return;
+      }
+      if ((password.length < 1) || (!password.match(alphaNumericRegEx))) {
+	  document.getElementById('output1').innerHTML = '<span style=color:red;>Password must not be empty and must consist of letters and numbers only</span>';
+	  return;
+      }
+
+      let myResponse = await fetch("userinfo.php", {
 	  method: 'POST',
 	  headers: {'Accept': 'application/json', 'Content-Type': 'application/json'},
 	  body: JSON.stringify({loginID: loginID, password: password})
 	  });
-  console.log("waiting")
-  let result = await myResponse.json();
-  console.log(result)
-  document.getElementById('output1').innerHTML = JSON.stringify(result);
-}
+      let result = await myResponse.json();
+      document.getElementById('output1').innerHTML = JSON.stringify(result);
+    
+  }
